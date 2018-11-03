3ba9bc325459c94e2c867a186d6da30e2f5c0f5f
Milos Prchlik <mprchlik@redhat.com>
2018-11-03 16:28:01 +0100

    Remove unused __init__.py stub

--- gluetool/__init__.pyi
+++
@@ -1,3 +0,0 @@
-from .glue import Failure as Failure, Glue as Glue, GlueCommandError as GlueCommandError, GlueError as GlueError, GlueRetryError as GlueRetryError, Module as Module
-from .glue import SoftGlueError as SoftGlueError
-from . import utils as utils
