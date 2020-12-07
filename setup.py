2f27058727d1b67e6110cb37ae8ef9ccd9fa6ba9
Miroslav Vadkerti <mvadkert@redhat.com>
2020-12-07 14:48:12 +0100

    Bump version to 1.25 (#116)
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 # setuptools-scm would extract version from a git tag, but one has to mention setuptools-scm in
 # `setup_requires` field, and pip does not play well with that one, and it's all kinds of messy
 # and I see no light :((
-VERSION = '1.24'
+VERSION = '1.25'
 
 
 if __name__ == '__main__':
